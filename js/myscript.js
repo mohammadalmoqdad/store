93999927a0fa9d39d25dfbab2019e52fa356a96e
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-09-22 23:24:13 +0300

    change the classes for each div

--- js/myscript.js
+++ js/myscript.js
@@ -4,21 +4,21 @@ table = prompt(" Do YOU NEED AN OFFICE DESKTOP? (please answer with yes and no)"
 plant=prompt("DO TOU NEED AN Amazonica Plant? (please answer with yes and no)");
 headphone=prompt("DO YOU NEED AN APPLE HEADPHONE? (please answer with yes and no)");
 
-if(table=='y' || table==yes ){
+if(table=='y' || table=='yes' ){
     $(document).ready(function(){
         $(".intro").css("background-color","yellow");
       });
 
 }
-if(plant=='y' || plant==yes ){
+if(plant=='y' || plant=='yes' ){
     $(document).ready(function(){
-        $(".intro").css("background-color","yellow");
+        $(".intro2").css("background-color","yellow");
       });
 
 }
-if(headphone=='y' || headphone==yes ){
+if(headphone=='y' || headphone=='yes' ){
     $(document).ready(function(){
-        $(".intro").css("background-color","yellow");
+        $(".intro3").css("background-color","yellow");
       });
 
 }
