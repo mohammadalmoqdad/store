df9db7fcf6f21ec532bfe4add7a5812096d104dc
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-09-22 23:15:28 +0300

    add js file

---
+++ js/myscript.js
@@ -0,0 +1,24 @@
+
+var table,plant,headphone;
+table = prompt(" Do YOU NEED AN OFFICE DESKTOP? (please answer with yes and no)");
+plant=prompt("DO TOU NEED AN Amazonica Plant? (please answer with yes and no)");
+headphone=prompt("DO YOU NEED AN APPLE HEADPHONE? (please answer with yes and no)");
+
+if(table=='y' || table==yes ){
+    $(document).ready(function(){
+        $(".intro").css("background-color","yellow");
+      });
+
+}
+if(plant=='y' || plant==yes ){
+    $(document).ready(function(){
+        $(".intro").css("background-color","yellow");
+      });
+
+}
+if(headphone=='y' || headphone==yes ){
+    $(document).ready(function(){
+        $(".intro").css("background-color","yellow");
+      });
+
+}
